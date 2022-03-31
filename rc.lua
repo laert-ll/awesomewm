11ba99280d18ed64d1bb45c15a62cdf6518af1a1
laert-ll <laertllaveshi@protonmail.com>
2022-03-31 17:22:13 +0200

    testing

--- rc.lua
+++ rc.lua
@@ -1,5 +1,8 @@
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+
+-- testing
+
 pcall(require, "luarocks.loader")
 
 -- Standard awesome library
